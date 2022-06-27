4dd857dba7bf344cadbe44c5219cbdf44832fcfd
Djhonatas Oliveira <dj.assuntospessoais@gmail.com>
2022-06-27 19:07:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portifolio from './components/portifolio/Portifolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
+const App = () => {
+  return (
+<>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portifolio />
+    <Testimonials />
+    <Contact />
+    <Footer />    
+</>
+  )
+}
+
+export default App
---
+++ src/components/portifolio/Portifolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './portifolio.css'
+
+const Portifolio = () => {
+  return (
+    <div>Portifolio</div>
+  )
+}
+
+export default Portifolio
